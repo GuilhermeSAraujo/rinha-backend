df3e48a5880f73d4632fc6392267c8f036b45a5e
Guilherme Souza Araújo <guilherme.souza@dtidigital.com.br>
2023-08-30 10:20:43 -0300

    buid: create table and docker-compose

--- ddl.sql
+++ ddl.sql
@@ -1,6 +1,6 @@
 CREATE TABLE pessoas (
     id UUID PRIMARY KEY,
-    apelido VARCHAR(32) NOT NULL UNIQUE,
+    apelido VARCHAR(32) NOT NULL,
     nome VARCHAR(100) NOT NULL,
     nascimento DATE NOT NULL,
     stack VARCHAR(255),
